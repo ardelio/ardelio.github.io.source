7ebaad88e0f0b02189e35b5bcbb479ceeb2f08a3
Anthony Sceresini <asceresini@gmail.com>
2022-04-03 14:30:14 +1000

    Add username

---
+++ src/components/pages/home/Username.js
@@ -0,0 +1,14 @@
+import { Stack, Typography } from '@mui/material';
+import React from 'react';
+
+export default function Username() {
+  return (
+    <Stack
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Typography variant='h3'>Ardelio</Typography>
+      <Typography>(Anthony Sceresini)</Typography>
+    </Stack>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,19 @@ import { Box, Container, Stack } from '@mui/material'
 import Avatar from '../components/pages/home/Avatar'
 import Features from '../components/pages/home/Features'
 import Socials from '../components/pages/home/Socials'
+import Username from '../components/pages/home/Username'
 
 export default function Index() {
+  const SPACER = <Box />
   return (
     <Container>
       <Stack
         spacing={6}
         sx={{ height: '100%', width: '100%' }}
       >
-        <Box></Box>
+        {SPACER}
         <Avatar />
+        <Username />
         <Socials />
         <Features />
       </Stack>
